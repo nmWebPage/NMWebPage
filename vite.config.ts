16797a0b8800fca78ea9677a63b2bf5d4ffc60ff
feLiruc <fim1991@hotmail.com>
2025-05-27 13:51:47 -0300

    Build

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,18 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/NMWebPage/',  // Ajuste conforme o nome do repositório
   server: {
     host: "::",
     port: 8080,
   },
+  build: {
+    outDir: 'docs'   // 👈 Saída do build vai para /docs
+  },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
